f29b914fc31e8e3d78323e0226af7ac918a358b9
jk1dd <portajonk@gmail.com>
2017-02-15 16:22:42 -0700

    add testing

--- test/refactored_server_test.rb
+++ test/refactored_server_test.rb
@@ -6,20 +6,40 @@ require 'minitest/pride'
 class ServerTest < Minitest::Test
 
   def test_responds_200
-    skip
     response = Faraday.get 'http://127.0.0.1:9292/'
     assert_equal 200, response.status
   end
 
   def test_returns_correct_hello_count
     response = Faraday.get 'http://127.0.0.1:9292/hello'
-    expected = "<html><head></head><body><h1> Hello World! (1) </h1></body></html>"
 
-    assert_equal expected, response.body
+    assert response.body.include?("Hello World! (1)")
+    # expected = "<html><head></head><body><h1> Hello World! (1) </h1></body></html>"
+    #
+    # assert_equal expected, response.body
 
     response = Faraday.get 'http://127.0.0.1:9292/hello'
-    expected = "<html><head></head><body><h1> Hello World! (2) </h1></body></html>"
-
-    assert_equal expected, response.body
+    # expected = "<html><head></head><body><h1> Hello World! (2) </h1></body></html>"
+    assert response.body.include?("Hello World! (2)")
+    # assert_equal expected, response.body
   end
+
+  def test_can_get_diagnostics
+    response = Faraday.get 'http://127.0.0.1:9292/'
+    expected = "<html><head></head>
+    <body>
+        <pre>Verb: GET
+Path: /
+Protocol: HTTP/1.1
+Host: 127.0.0.1
+Port:9292
+Origin: 127.0.0.1
+Accept: */*</pre>
+    </body>
+</html>"
+
+    assert expected, response.body
+  end
+
+  def 
 end
